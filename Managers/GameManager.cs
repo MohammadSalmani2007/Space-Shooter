990781640fcbed9fa25abef195175880b2a0858a
Mohammad <mohammad.salmani7.1386@gmail.com>
2026-06-27 20:27:08 +0330

    Initial implementation of GameManager class

---
+++ Managers/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using AP_Final_Project.Characters;
+using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Text;
+
+namespace AP_Final_Project.Managers
+{
+    public class GameManager//All the logic is here :)
+    {
+        public Player MainPlayer { get; private set; }
+        public List<Enemy> ActiveEnemies { get; private set; }
+        public List<Bullet> ActiveBullets { get; private set; }
+
+        private int gameWidth;
+        private int gameHeight;
+
+
+        public GameManager(int windowWidth, int windowHeight)
+        {
+            gameWidth = windowWidth;
+            gameHeight = windowHeight;
+
+            MainPlayer = new Player(gameWidth/2 - 25, gameHeight - 80);//Assuming a position for player (middle-down of game window)
+            ActiveEnemies = new List<Enemy>();
+            ActiveBullets = new List<Bullet>();
+        }
+
+        public void Update()
+        {
+            MainPlayer.Update();
+            ApplyBoundryChecking();
+        }
+        
+        public void Draw(Graphics g)//Draw must be out of Game Form !!
+        {
+            MainPlayer.Draw(g);//Must be implement in Player.cs/and others...
+        }
+
+        private void ApplyBoundryChecking()//Locking the player in the game window.
+        {
+            if (MainPlayer.X < 0) MainPlayer.X = 0;
+            if(MainPlayer.X > gameWidth - MainPlayer.Width) MainPlayer.X = gameWidth - MainPlayer.Width;
+            if (MainPlayer.Y < 0) MainPlayer.Y = 0;
+            if(MainPlayer.Y > gameHeight - MainPlayer.Height) MainPlayer.Y = gameHeight - MainPlayer.Height;
+        }
+    }
+}
