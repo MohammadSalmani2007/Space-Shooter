042770c4a8130298035fbd87ac2789983bfbdd9e
Mohammad <mohammad.salmani7.1386@gmail.com>
2026-06-27 18:43:30 +0330

    Implement abstract Entity base class

---
+++ Characters/Entity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AP_Final_Project.Characters
+{
+    public abstract class Entity
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width {  get; set; }
+        public int Height { get; set; }
+        
+
+        public int Speed {  get; set; }
+        public int HP {  get; set; }
+
+
+        protected Entity(int x, int y, int width, int height, int speed, int hP)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Speed = speed;
+            HP = hP;
+        }
+
+        public abstract void Update();
+
+        public virtual void Draw(Graphics g)
+        {
+
+        }
+    }
+}
