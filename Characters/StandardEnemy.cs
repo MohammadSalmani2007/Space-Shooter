5116bc9ef36dcc698da004861ec6f3172a34cf48
Mohammad <mohammad.salmani7.1386@gmail.com>
2026-06-28 12:45:44 +0330

    Implement StandardEnemy class

---
+++ Characters/StandardEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AP_Final_Project.Characters
+{
+    public class StandardEnemy : Enemy
+    {
+        public StandardEnemy(int x, int y)
+            : base (x, y , width: 40, height: 40, speed: 3, hp: 1, scoreValue: 10)//Assuming values to move the project forward 
+        {
+        }
+
+        public override void Update()
+        {
+            Y += Speed;//Moving downward
+        }
+
+        public override void Draw(Graphics g)
+        {
+
+        }
+    }
+}
