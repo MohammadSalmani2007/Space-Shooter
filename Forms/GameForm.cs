8b26e0f42e2743087b2975c9c4a0c73dd3c3e788
Mohammad <mohammad.salmani7.1386@gmail.com>
2026-06-28 18:39:18 +0330

    Add KeyDown and KeyUP events

--- Forms/GameForm.cs
+++ Forms/GameForm.cs
@@ -29,6 +29,8 @@ namespace AP_Final_Project.Forms
 
             //Conecting the event(s) through event handlers
             this.Paint += new PaintEventHandler(GameForm_Paint);
+            this.KeyDown += new KeyEventHandler(GameForm_KeyDown);
+            this.KeyUp += new KeyEventHandler(GameForm_KeyUp);
 
             //Game Loop starts with adding game timer event !
             gameTimer = new System.Windows.Forms.Timer();
@@ -39,11 +41,33 @@ namespace AP_Final_Project.Forms
         private void GameTimer_Tick(object? sender, EventArgs e)
         {
             gameManager.Update();
+            this.Invalidate();//Redraw
         }
 
         private void GameForm_Paint(object? sender, PaintEventArgs e)
         {
             gameManager.Draw(e.Graphics);
+        }
+
+        private void GameForm_KeyDown(object? sender, KeyEventArgs e)
+        {
+            var player = gameManager.MainPlayer;
+            //both arrows and WASD keys
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A) player.IsMovingLeft = true;
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D) player.IsMovingRight = true;
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W) player.IsMovingUp = true;
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S) player.IsMovingDown = true;
+        }
+
+        private void GameForm_KeyUp(object? sender, KeyEventArgs e)
+        {
+            var player = gameManager.MainPlayer;
+            //both arrows and WASD keys
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A) player.IsMovingLeft = false;
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D) player.IsMovingRight = false;
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W) player.IsMovingUp = false;
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S) player.IsMovingDown = false;
+
         }
 
         private void GameForm_Load(object sender, EventArgs e)
